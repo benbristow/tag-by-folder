b6f782eaf8e9b6f87367ad42340dca729b965972
Ben Bristow <me@benbristow.co.uk>
2017-04-12 17:14:10 +0100

    Updates to how tagging is done

--- main.rb
+++ main.rb
@@ -13,23 +13,41 @@ Escort::App.create do |app|
     directory = arguments[0]
     abort "Directory does not exist" unless File.directory?(directory)
 
+    puts "> Scanning #{directory} for MP3 files and checking genres..."
     track_paths = Dir.glob("#{directory}/**/*.mp3")
     abort "No files in directory" if track_paths.empty?
 
     changes = 0
     track_paths.each do |track_path|
-      directory_name = track_path.split('/')[-2]
-      TagLib::MPEG::File.open(track_path) do |track|
-        tag = track.id3v2_tag
-        if tag.genre != directory_name
-          puts "> Replacing genre for #{File.basename(track_path)} to #{directory_name}"
-          tag.genre = directory_name
-          track.save
-          changes += 1
+      open_track(track_path) do |track, tag|
+        if track_genre(track) != intended_genre(track_path)
+          replace_genre(track, intended_genre(track_path))
         end
       end
     end
 
     abort "Finished! #{changes} tag changes made overall."
+  end
+
+  private
+
+  def open_track(track_path)
+    TagLib::MPEG::File.open(track_path) do |track|
+      yield track
+    end
+  end
+
+  def track_genre(track)
+    track.tag.genre
+  end
+
+  def intended_genre(track_path)
+    track_path.split('/')[-2]
+  end
+
+  def replace_genre(track, genre)
+    track.tag.genre = genre
+    track.save
+    puts "Set #{track.tag.title}'s genre to #{genre}"
   end
 end
